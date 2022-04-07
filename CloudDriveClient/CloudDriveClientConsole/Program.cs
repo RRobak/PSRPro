ab572164fcfcb7c1f5038b5c9b433e31285d045b
unknown <rafal.robak@interia.pl>
2022-04-07 10:40:38 +0200

    WCF Client Server communication via TCP sockets hosted in console

---
+++ CloudDriveClient/CloudDriveClientConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudDriveClientConsole.CloudDriveClientReference;
+namespace CloudDriveClientConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudDriveClientServiceClient client = new CloudDriveClientServiceClient();
+            string message;
+            Console.WriteLine("Podaj wiadmosc");
+            message=Console.ReadLine();
+            message=client.SendMessage(message);
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ CloudDriveClient/CoudDriveClientHost/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using CloudDriveClient;
+using System.ServiceModel.Description;
+
+namespace CoudDriveClientHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tworzymy adres gdzie pod którym będzie dostępna usługa
+            Uri baseAddress = new Uri("http://localhost:8733/CloudDriveClientService/");
+
+            // Tworzymy obiekt klasy CalculatorService
+            ServiceHost selfHost = new ServiceHost(typeof(CloudDriveClientService), baseAddress);
+
+            try
+            {
+                // Dodajemy Endopoint usługi
+                selfHost.AddServiceEndpoint(typeof(ICloudDriveClientService), new WSHttpBinding(), "CalculatorService");
+
+                // Umożliwiamy wymianę metadanych
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                // Startujemy serwis
+                selfHost.Open();
+                Console.WriteLine("Serwis działa....");
+                Console.WriteLine("Naciśnij <ENTER> by zakończyć.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                // zamykamy serwis
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("Przechwyciłem wyjątek: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
